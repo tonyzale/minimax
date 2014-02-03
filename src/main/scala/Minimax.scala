d3001c6e0057f6237f83120cbefc630ffaf29cb8
tonyzale <tonyzale@gmail.com>
2014-02-03 15:40:27 -0600

    MinimaxNode eval logic w/ working lazy child eval

--- src/main/scala/Minimax.scala
+++ src/main/scala/Minimax.scala
@@ -1,14 +1,31 @@
-package com.tonyzale.minimax
+package minimax
+
+object Minimax {
+  type PlayerId = Int
+}
 
 trait GameState {
-  def evalForPlayer(player: Int): Double
-  def validMoves(player: Int): List[this.type]
+  def whosTurn: Minimax.PlayerId
+  def evalForPlayer(pid: Minimax.PlayerId): Double
+  def validMoves: List[GameState]
   def uid: Long
 }
 
 class MiniMaxNode (val state: GameState){
-  val children: List[MiniMaxNode] = Nil
-  
+  lazy val children: List[MiniMaxNode] = state.validMoves.map(new MiniMaxNode(_))
+  def Eval(depth: Int, pid: Minimax.PlayerId): Double = {
+    if (depth == 0) state.evalForPlayer(pid)
+    // TODO: fix double eval of winning state
+    else if (state.whosTurn == pid) {
+      children.maxBy(_.Eval(depth - 1, pid)).state.evalForPlayer(pid)
+    } else {
+      children.minBy(_.Eval(depth - 1, pid)).state.evalForPlayer(pid)
+    }
+  }
+}
+
+class MiniMaxTree(state: GameState, pid: Minimax.PlayerId) {
+  val root = new MiniMaxNode(state)
 }
 
 object Hello extends App {
--- src/test/scala/MinimaxTest.scala
+++ src/test/scala/MinimaxTest.scala
@@ -1,18 +1,40 @@
-package com.tonyzale.minimax
+package minimax
 
 import org.scalatest.FunSuite
- 
-class MinimaxSuite extends FunSuite {  
-  class TestState extends GameState {
-    def evalForPlayer(id: Int): Double = 0
+//import org.junit.runner.RunWith
+//import org.scalatest.junit.JUnitRunner
+
+  class TestState(ctorCallback: ()=> Unit) extends GameState {
+    def whosTurn: Minimax.PlayerId = 0
+    def evalForPlayer(id: Minimax.PlayerId): Double = 0
     def uid(): Long = 0
-    def validMoves(id: Int): List[this.type] = Nil
+    def validMoves: List[GameState] = new TestState(ctorCallback) :: new TestState(ctorCallback) :: Nil
+    ctorCallback()
   }
-  
-  test("minimax test") {
-    val testState = new TestState
+
+//@RunWith(classOf[JUnitRunner])
+class MinimaxSuite extends FunSuite {    
+  test("minimax lazy child evaluation works correctly") {
+    var ctorCount = 0
+    val testState = new TestState(() => ctorCount += 1)
     val n = new MiniMaxNode(testState)
     assert(n.state == testState)
+    assert(ctorCount == 1)
+    n.Eval(0, 0)
+    assert(ctorCount == 1)
+    n.Eval(1, 0)
+    assert(ctorCount == 3)
   }
 }
 
+object TestMain extends App {
+    var ctorCount = 0
+    val testState = new TestState(() => ctorCount += 1)
+    val n = new MiniMaxNode(testState)
+    assert(n.state == testState)
+    assert(ctorCount == 1)
+    n.Eval(0, 0)
+    assert(ctorCount == 1)
+    n.Eval(1, 0)
+    assert(ctorCount == 3)
+}
