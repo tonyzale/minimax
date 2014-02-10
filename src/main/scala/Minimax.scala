4c1c86f7bafa5265fdc1413f3b56d1f5660617b9
tonyzale <tonyzale@gmail.com>
2014-02-09 20:44:24 -0600

    fixed tree eval

--- src/main/scala/Minimax.scala
+++ src/main/scala/Minimax.scala
@@ -9,19 +9,59 @@ trait GameState {
   def evalForPlayer(pid: Minimax.PlayerId): Double
   def validMoves: List[GameState]
   def uid: Long
+  override def toString: String = {s"GameState Id $uid $whosTurn ${evalForPlayer(0)}" }
 }
 
 class MiniMaxNode (val state: GameState){
   lazy val children: List[MiniMaxNode] = state.validMoves.map(new MiniMaxNode(_))
-  def Eval(depth: Int, pid: Minimax.PlayerId): GameState = {
-    if (depth == 0) state
-    // TODO: fix double eval of winning state
-    else if (state.whosTurn == pid) {
-      children.maxBy(_.Eval(depth - 1, pid).evalForPlayer(pid)).state
+  lazy val childrenWithEval: List[(MiniMaxNode, Double)] = state.validMoves.map(el => (new MiniMaxNode(el), 0.0))
+  def Eval(depth: Int, pid: Minimax.PlayerId): (GameState, Double) = {
+    if (depth == 0) {
+      println("depth 0 with: " + state)
+      (state, state.evalForPlayer(pid))
+    } else if (state.whosTurn == pid) {
+      println(s"max eval of $state")
+      println(s"children $childrenWithEval")
+      //val result = children.maxBy(_.Eval(depth - 1, pid)._2)
+      
+      val foldResult = childrenWithEval.fold((null, 0.0))((a, b) => {
+        if (a._1 == null) b
+        else {
+          val aResult = a._1.Eval(depth - 1, pid)
+          val bResult = b._1.Eval(depth - 1, pid)
+          if (aResult._2 > bResult._2) {
+            (a._1, aResult._2)
+          } else {
+            (b._1, bResult._2)
+          }
+        }
+      })
+      
+      println(s"**result for $state : $foldResult")
+      (foldResult._1.state, foldResult._2)
     } else {
-      children.minBy(_.Eval(depth - 1, pid).evalForPlayer(pid)).state
+      println(s"min eval of $state")
+      println(s"children childrenWithEval")
+      //val result = children.minBy(_.Eval(depth - 1, pid)._2).state
+      
+      val foldResult = childrenWithEval.fold((null, 0.0))((a, b) => {
+        if (a._1 == null) b
+        else {
+          val aResult = a._1.Eval(depth - 1, pid)
+          val bResult = b._1.Eval(depth - 1, pid)
+          if (aResult._2 < bResult._2) {
+            (a._1, aResult._2)
+          } else {
+            (b._1, bResult._2)
+          }
+        }
+      })
+      
+      println(s"**result for $state : $foldResult")
+      (foldResult._1.state, foldResult._2)
     }
   }
+  override def toString: String = {s"Node w/ $state"}
 }
 
 class MiniMaxTree(state: GameState, pid: Minimax.PlayerId) {
--- src/test/scala/MinimaxTest.scala
+++ src/test/scala/MinimaxTest.scala
@@ -8,12 +8,12 @@ import org.scalatest.FunSuite
     def whosTurn: Minimax.PlayerId = 0
     def evalForPlayer(id: Minimax.PlayerId): Double = 0
     def uid(): Long = 0
-    def validMoves: List[GameState] = new TestState(ctorCallback) :: new TestState(ctorCallback) :: Nil
+    def validMoves: List[GameState] = List(new TestState(ctorCallback), new TestState(ctorCallback))
     ctorCallback()
   }
 
 //@RunWith(classOf[JUnitRunner])
-class MinimaxSuite extends FunSuite {    
+class MinimaxSuite extends FunSuite {
   test("minimax lazy child evaluation works correctly") {
     var ctorCount = 0
     val testState = new TestState(() => ctorCount += 1)
@@ -26,20 +26,52 @@ class MinimaxSuite extends FunSuite {
     assert(ctorCount == 3)
   }
   
+  var constEvalStateCount = 0
+      class ConstEvalState(playerTurn: Minimax.PlayerId, eval: Double, var children: List[ConstEvalState]) extends GameState {
+      def this(playerTurn: Minimax.PlayerId, eval: Double) = this(playerTurn, eval, Nil)
+      def this(playerTurn: Minimax.PlayerId) = this(playerTurn, 0.0, Nil)
+      def whosTurn: Minimax.PlayerId = playerTurn
+      def evalForPlayer(id: Minimax.PlayerId): Double = eval
+      def uid(): Long = myId
+      def validMoves: List[GameState] = children   
+      val myId = constEvalStateCount
+      constEvalStateCount = constEvalStateCount + 1
+    }
+  
   //                 Root
   // my moves    1.0      0
   // opp moves   -1.0     0.5
   // should choose "0"
-  test("minimax chooses correct move") {
-    class ConstEvalState(eval: Double, children: List[ConstEvalState]) extends GameState {
-      def whosTurn: Minimax.PlayerId = 0
-      def evalForPlayer(id: Minimax.PlayerId): Double = eval
-      def uid(): Long = 0
-      def validMoves: List[GameState] = children      
-    }
-    
-    val root = new MiniMaxNode(new ConstEvalState(0.0,
-        new ConstEvalState(1.0, new ConstEvalState(-1.0, Nil) :: Nil) :: new ConstEvalState(0.0, new ConstEvalState(0.5, Nil) :: Nil) :: Nil))
-    assert(root.Eval(2, 0) == root.children.toVector(1).state)
+
+  test("simple minimax chooses correct move") {    
+    val rootState = new ConstEvalState(0)
+    val a1 = new ConstEvalState(1, 1.0, List(new ConstEvalState(0, -1.0)))
+    val a2 = new ConstEvalState(1, 0.0, List(new ConstEvalState(0, 0.5)))
+    rootState.children = List(a1, a2)
+    val root = new MiniMaxNode(rootState)
+    assert(root.Eval(2, 0)._1 == a2)
+  }
+
+  test("minimax solves wikipedia example") {
+    val root2 = new ConstEvalState(0)
+    val l1c = List.fill(2)(new ConstEvalState(1))
+    root2.children = l1c
+    val l2c = List.fill(4)(new ConstEvalState(0))
+    l1c(0).children = l2c.take(2)
+    l1c(1).children = l2c.drop(2)
+    val l3c = List.fill(6)(new ConstEvalState(1))
+    l2c(0).children = l3c.take(2)
+    l2c(1).children = l3c.slice(2, 3)
+    l2c(2).children = l3c.slice(3, 5)
+    l2c(3).children = l3c.slice(5, 6)
+    val l4c = List(new ConstEvalState(0, 10), new ConstEvalState(0, 1000), new ConstEvalState(0, 5), new ConstEvalState(0, -10),
+                              new ConstEvalState(0, 7), new ConstEvalState(0, 5), new ConstEvalState(0, -1000), new ConstEvalState(0, -7), new ConstEvalState(0, -5))
+    l3c(0).children = l4c.take(2)
+    l3c(1).children = List(l4c(2))
+    l3c(2).children = List(l4c(3))
+    l3c(3).children = l4c.slice(4, 6)
+    l3c(4).children = List(l4c(6))
+    l3c(5).children = l4c.takeRight(2)
+    assert(new MiniMaxNode(root2).Eval(4, 0)._1 == l1c(1))
   }
 }
